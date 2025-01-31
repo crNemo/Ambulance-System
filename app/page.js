02db2a9bfb92a06cded529afbdb14d5e9e529147
Bibash Thapa <crnemo07@gmail.com>
2025-01-31 13:58:42 +0545

    Done for now

--- app/page.js
+++ app/page.js
@@ -1,15 +1,27 @@
+"use client";
 import GoogleMapSection from "@/components/Home/GoogleMapSection";
 import SearchSection from "@/components/Home/SearchSection";
+import { DestincationContext } from "@/context/DestinationContext";
+import { SourceContext } from "@/context/SourceContext";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [source,setSource]=useState([]);
+  const [destination,setDestination]=useState([]);
+
   return (
-    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
-      <div className="md:col-span-1">
-        <SearchSection />
+    <SourceContext.Provider value={{source,setSource}}>
+      <DestincationContext.Provider value={{destination,setDestination}}>
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className="md:col-span-1">
+          <SearchSection />
+        </div>
+        <div className="md:col-span-2">
+          <GoogleMapSection />
+        </div>
       </div>
-      <div className="md:col-span-2">
-        <GoogleMapSection />
-      </div>
-    </div>
+      </DestincationContext.Provider>
+    </SourceContext.Provider>
   );
 }
--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -1,7 +1,23 @@
-import React from 'react'
+"use client";
+import React, { useContext, useEffect } from 'react'
 import InputItem from './InputItem'
+import { SourceContext } from '@/context/SourceContext'
+import { DestincationContext } from '@/context/DestinationContext'
 
 const SearchSection = () => {
+
+  const {source,setSource} = useContext(SourceContext);
+  const {destination,setDestination} = useContext(DestincationContext);
+
+  useEffect(() => {
+    if(source){
+      console.log(source);
+    }
+    if(destination){
+      console.log(destination);
+    }
+  },[source, destination]);
+
   return (
     <div className='p-2 md:pd-5 border-[2px] rounded-xl'>
         <p className='text-[20px] font-bold'>Get Ambulance</p>
